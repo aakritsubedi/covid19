09227417943e947a57a27572d17c5c4841d02c1c
samir <samirsilwal2424@gmail.com>
2020-05-26 19:22:02 +0545

    Add summary for each district

--- src/stores/modules/nepal.js
+++ src/stores/modules/nepal.js
@@ -15,7 +15,7 @@ const getters = {
 };
 
 const actions = {
-  async getNepalInfo({ commit }) {
+  async getNepalInfo({ commit, dispatch }) {
     let nepalData = await api.nepalData();
     let result = {
       "Tested Positive": nepalData.tested_positive,
@@ -28,13 +28,58 @@ const actions = {
       "Recovered": nepalData.recovered,
       "Deaths": nepalData.deaths,
     }
-
+    dispatch('getDistrictInfo')
     commit('setNepalInfo', result);
   },
 
   async getDistrictInfo({ commit }) {
     let districtInfo = await api.districtData();
+
     const districtInfoMap = districtInfo.map(data => {
+      if (data.covid_cases.length != 0) {
+        const covidCasesData = data.covid_cases.map(data => {
+          return {
+            age: data.age,
+            state: data.currentState,
+            gender: data.gender
+          }
+        })
+
+        const summary = getSummary(covidCasesData)
+        return {
+          title: data.title,
+          title_ne: data.title_ne,
+          covid_cases: data.covid_cases,
+          summary: summary,
+          province: data.province
+        }
+
+      }
+      function getSummary(data) {
+        const ages = data.map(info => info.age).filter(data => data != null)
+        const state = data.map(info => info.state).filter(data => data != null)
+        const gender = data.map(info => info.gender).filter(data => data != null)
+
+        var min, max;
+        if (ages.length == 0) {
+          min = 0;
+          max = 0;
+        } else {
+          min = Math.min(...ages)
+          max = Math.max(...ages)
+        }
+
+        return {
+        active :state.filter(data => data == "active").length,
+        inactive : state.filter(data => data == "recovered").length,
+        
+        male : gender.filter(data => data == "male").length,
+        female :gender.filter(data => data == "female").length,
+        minAge: min,
+        maxAge: max
+        }
+
+      }
 
       return {
         title: data.title,
@@ -43,7 +88,6 @@ const actions = {
         province: data.province
       }
     });
-    console.log(districtInfoMap)
 
     commit('setDistrictInfo', districtInfoMap);
   },
