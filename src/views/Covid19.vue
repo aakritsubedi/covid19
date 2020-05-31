ee858c1ec37a829d3adfeb7c6ff3edd9b1e21dd1
monzilnepali <manjilnepali98@gmail.com>
2020-05-31 11:59:49 +0545

    Add covid Checker Design

--- src/views/Covid19.vue
+++ src/views/Covid19.vue
@@ -1,102 +1,125 @@
 <template>
-  <div class="container-fluid" style="background: #f4f4f4; height:100vh;">
-    <img src="@/assets/images/covid.png" alt />
-    <div class="test-container">
-      <h2>
-        COVID-19
-        <br />Screening Tool
-      </h2>
-      <!-- <p>This tool can help you understand what to do next about COVID-19.</p>
-      <p>Let’s all look out for each other by knowing our status, trying not to infect others, and reserving care for those in need.</p>
+  <div class="covid-container ">
+    <div class="covid__header">
+      <h1>Check your risk for COVID‑19</h1>
+    </div>
 
-      <button>Start Screening</button>-->
+    <div class="d-flex f-j-center">
+      <div class="col-lg-5">
+        <div class="covid__body">
+          <div class="first-scene" v-if="!tempStatus">
+            <p class="label">
+              Based on best clinical practices, CDC guidelines, illness severity
+              and risk factors like age and pre-existing conditions.
+            </p>
+            <div class="covid-btn" @click="changeScene()">Get Started</div>
+          </div>
+          <div class="covid-question" v-if="tempStatus">
+            <div class="title">
+              Have you recently started experiencing any of these symptoms?
+            </div>
+            <div class="options">
+              <li class="options__item">Fever or chills</li>
+              <li class="options__item options__item--active">
+                Mild or Moderate difficulty breathing
+              </li>
+              <li class="options__item options__item--active">
+                ew or worsening cough
+              </li>
+              <li class="options__item options__item--active">sore throat</li>
+              <li class="options__item">None of the above</li>
+              <li class="options__item">None of the above</li>
+              <li class="options__item">None of the above</li>
 
-      <div>
-        <div>{{question.question}}</div>
-        <ul id="qlist">
-          <li v-for="(option, key) in question.options" :key="(key+1) * (Math.random() * 10 + 125)">
-            {{option.value}}
-            <input
-              type="checkbox"
-              :id="(key+1) * (Math.random() * 10 + 125)"
-              v-model="option.selected"
-            />
-          </li>
-        </ul>
+              <li class="options__item">
+                In the last 14 days, what is your exposure to others who are
+                known to have COVID‑19
+              </li>
+            </div>
+            <div class="footer">
+              <div class="footer-btn inactive">Back</div>
+              <div class="footer-btn active">Next</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <button @click="nextHandler()">next</button>
-      <button @click="backHandler()" v-show="result == 0">back</button>
     </div>
   </div>
 </template>
 
 <script>
-import { questions } from "@/constants/covidQue.js";
+import { questions } from '@/constants/covidQue.js'
 
 export default {
-  name: "Covid19",
+  name: 'Covid19',
   data() {
     return {
       questions: [],
       selectedIndex: 0,
       finished: false,
-      result:0
-    };
+      result: 0,
+      tempStatus: false,
+    }
   },
   methods: {
+    changeScene: function() {
+      console.log('chagnes')
+      this.tempStatus = true
+    },
     init: function() {
-      this.questions = questions;
+      this.questions = questions
     },
     backHandler: function() {
-      this.finished = false;
+      this.finished = false
       if (this.selectedIndex > 0) {
-        this.selectedIndex -= 1;
+        this.selectedIndex -= 1
       }
     },
-    computeResult: function(){
-      var result=0;
+    computeResult: function() {
+      var result = 0
       this.questions.forEach((item) => {
-        var total=0
-        item.options.forEach(opt => {
-          if (opt.selected){
+        var total = 0
+        item.options.forEach((opt) => {
+          if (opt.selected) {
             total += opt.points
           }
         })
         result += total * item.factor
-      });
+      })
       this.result = result
       console.log(this.result)
     },
     nextHandler: function() {
       if (this.finished == true) {
         this.computeResult()
-        return;
+        return
       }
 
       if (this.selectedIndex < questions.length) {
-        this.selectedIndex += 1;
+        this.selectedIndex += 1
         if (this.selectedIndex == questions.length - 1) {
-          this.finished = true;
+          this.finished = true
         }
       }
-    }
+    },
   },
   computed: {
     question: function() {
-      return this.questions[this.selectedIndex];
-    }
+      return this.questions[this.selectedIndex]
+    },
   },
+
   created() {
-    this.init();
-  }
-};
+    this.init()
+  },
+}
 </script>
 
 <style>
 .test-container {
   display: flex;
   flex-direction: column;
-  background-image: url("../assets/images/covidTest.png");
+  background-image: url('../assets/images/covidTest.png');
   background-position: right;
   height: 500px;
   width: 80%;
