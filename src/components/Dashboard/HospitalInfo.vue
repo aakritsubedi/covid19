e0e648d63b0f2b961ff4fd987d262ca1571aee71
monzilnepali <manjilnepali98@gmail.com>
2020-05-31 12:26:47 +0545

    Fix issue to table view in mobile device

--- src/components/Dashboard/HospitalInfo.vue
+++ src/components/Dashboard/HospitalInfo.vue
@@ -1,79 +1,85 @@
 <template>
-  <div style="width:80%; margin:0 auto;">
+  <div style="width:90%; margin:0 auto;">
     <div class="newrow country-select mt-5">
       <div class="col-xs-12">
-        <input type="text" class="form-control" placeholder="Hospital Name" v-model="search" />
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Hospital Name"
+          v-model="search"
+        />
       </div>
     </div>
-    <table class="table mt-3">
-      <thead>
-        <tr>
-          <th width="40%">Hospital Info</th>
-          <th width="20%">Contact Info.</th>
-          <th width="40%">Info</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(hospital, index) in filterHospital" :key="index">
-          <td>
-            <ul class="hospital-list">
-              <li>{{ hospital.name }}</li>
-              <li><b>{{ hospital.address }}</b> Province-{{hospital.state}}</li>
-            </ul>
-          </td>
-          <td>
-            <ul class="hospital-list">
-              <li>{{ hospital.phone || '-' }} </li>
-              <li v-if="hospital.contact_person">
-                {{ hospital.contact_person }}, {{ hospital.contact_person_number }}
-              </li>
-            </ul>
-          </td>
-          <td>
-            <ul class="hospital-list">
-              <li>
-                No. of beds: {{ hospital.capacity.beds || 0 }}
-              </li>
-              <li class="text-capitalize">
-                Vantilator: {{hospital.capacity.ventilators || 'no' }}
-              </li>
-              <li>
-                Isolation Beds: {{hospital.capacity.isolation_beds || '-'}}
-              </li>
-            </ul>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div style="overflow-x:auto;">
+      <table class="table mt-3">
+        <thead>
+          <tr>
+            <th width="40%">Hospital Info</th>
+            <th width="20%">Contact Info.</th>
+            <th width="40%">Info</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(hospital, index) in filterHospital" :key="index">
+            <td>
+              <div class="hospital-list">
+                <li>{{ hospital.name }}</li>
+                <li>
+                  <b>{{ hospital.address }}</b> Province-{{ hospital.state }}
+                </li>
+              </div>
+            </td>
+            <td>
+              <div class="hospital-list">
+                <li>{{ hospital.phone || '-' }}</li>
+                <li v-if="hospital.contact_person">
+                  {{ hospital.contact_person }},
+                  {{ hospital.contact_person_number }}
+                </li>
+              </div>
+            </td>
+            <td>
+              <div class="hospital-list">
+                <li>No. of beds: {{ hospital.capacity.beds || 0 }}</li>
+                <li class="text-capitalize">
+                  Vantilator: {{ hospital.capacity.ventilators || 'no' }}
+                </li>
+                <li>
+                  Isolation Beds: {{ hospital.capacity.isolation_beds || '-' }}
+                </li>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
-  name: "NepalHospital",
+  name: 'NepalHospital',
   data() {
     return {
-      search: ""
-    };
+      search: '',
+    }
   },
   methods: {
-    ...mapActions(["getNepalHospital"])
+    ...mapActions(['getNepalHospital']),
   },
   created() {
-    this.getNepalHospital();  
+    this.getNepalHospital()
   },
   computed: {
-    ...mapGetters(["nepalHospital"]),
+    ...mapGetters(['nepalHospital']),
     filterHospital: function() {
-
-      return this.nepalHospital.filter(hospital => { 
-        let keyword = this.search.toUpperCase();     
-        return hospital.name.match(keyword);
-      });
-    }
-  }
-};
+      return this.nepalHospital.filter((hospital) => {
+        let keyword = this.search.toUpperCase()
+        return hospital.name.match(keyword)
+      })
+    },
+  },
+}
 </script>
-
